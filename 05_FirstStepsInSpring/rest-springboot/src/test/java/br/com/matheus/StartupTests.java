02f2db9a50ac60bfa24dd01c9163476f37fe327f
Matheus Lira <mathgomes16@gmail.com>
2024-05-22 09:28:18 -0300

    Primeiros passos no Spring Boot 3

---
+++ 05_FirstStepsInSpring/rest-springboot/src/test/java/br/com/matheus/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.matheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
